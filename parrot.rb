6722fb19063566488dbdebe674eab1447b36f930
Jason Pitts <juniorjr083@gmail.com>
2018-05-30 01:53:23 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk!")
-  if phrase  == "Squawk!" ?  
+  if phrase  == "Squawk!"   
   return "Squawk"
 else 
   puts phrase
